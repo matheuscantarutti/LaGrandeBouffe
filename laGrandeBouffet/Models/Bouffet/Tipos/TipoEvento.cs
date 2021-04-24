342a4c654f9fa7fb31f04496f0295472e1435905
MatheusABC <matheuscantarutti@gmail.com>
2021-04-24 15:02:42 -0300

    criação da classe TipoEvento

---
+++ laGrandeBouffet/Models/Bouffet/Tipos/TipoEvento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laGrandeBouffet.Models.Bouffet.Tipos
+{
+    public class TipoEvento
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+
+        public TipoEvento()
+        {
+            Id = new Guid();
+        }
+    }
+}
