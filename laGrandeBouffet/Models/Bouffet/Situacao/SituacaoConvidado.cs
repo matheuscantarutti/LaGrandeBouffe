0ec73170be301384e84fd840954fee3dd47bbbc2
MatheusABC <matheuscantarutti@gmail.com>
2021-04-24 15:07:16 -0300

    criacao da classe SituacaoConvidado

---
+++ laGrandeBouffet/Models/Bouffet/Situacao/SituacaoConvidado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laGrandeBouffet.Models.Bouffet.Situacao
+{
+    public class SituacaoConvidado
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+
+        public SituacaoConvidado()
+        {
+            Id = new Guid();
+        }
+    }
+}
