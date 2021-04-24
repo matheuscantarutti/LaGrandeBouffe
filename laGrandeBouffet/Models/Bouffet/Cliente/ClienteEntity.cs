8125f1a85c6ea6d3546cd155fcc8ba4672dce24c
MatheusABC <matheuscantarutti@gmail.com>
2021-04-24 15:33:15 -0300

    configuração da classe ClienteEntity

--- laGrandeBouffet/Models/Bouffet/Cliente/ClienteEntity.cs
+++ laGrandeBouffet/Models/Bouffet/Cliente/ClienteEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Buffet.Models.Buffet.Evento;
+using laGrandeBouffet.Models.Bouffet.Tipos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,40 @@ namespace La_Grande_Bouffe.Models
     public class ClienteEntity
     {
         public Guid Id { get; set; }
-
+        public TipoCliente Pessoa { get; set; }
         public String Nome { get; set; }
-
+        public int Cpf { get; set; }
+        public int Cnpj { get; set; }
         public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Obs { get; set; }
+        public DateTime DataNasicmento { get; set; }
+        public DateTime Criacao { get; set; }
+        public DateTime LastUpdate { get; set; }
         public ICollection<EventoEntity> Eventos { get; set; }
 
-        public ClienteEntity()
+        public ClienteEntity(TipoCliente pessoa, string nome, string email, string endereco, int cpf, DateTime dataNascimento)
         {
             Id = new Guid();
+            this.Criacao = DateTime.Now;
+            this.Nome = nome;
+            this.Email = email;
+            this.Endereco = endereco;
+            this.Cpf = cpf;
+            this.DataNasicmento = dataNascimento;
+
+        }
+        
+        public ClienteEntity(TipoCliente pessoa, string nome, string email, string endereco, int cnpj)
+        {
+            Id = new Guid();
+            this.Criacao = DateTime.Now;
+            this.Nome = nome;
+            this.Email = email;
+            this.Endereco = endereco;
+            this.Cnpj = cnpj;
+            
+
         }
 
     }
