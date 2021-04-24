594d1b6e48de7f1f4a7e90ef3e4c19535b037214
MatheusABC <matheuscantarutti@gmail.com>
2021-04-24 15:00:39 -0300

    criação da classe TipoCliente

---
+++ laGrandeBouffet/Models/Bouffet/Tipos/TipoCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laGrandeBouffet.Models.Bouffet.Tipos
+{
+    public class TipoCliente
+    {
+        public Guid Id { get; set; }
+        public string Descricao{ get; set; }
+
+        public TipoCliente()
+        {
+            Id = new Guid();
+        }
+    }
+}
